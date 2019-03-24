9dc2139ff59a7e7064459c4ac2d3a72866ed197a
Jean Bubu <jeanbubu92@gmail.com>
2019-03-24 12:47:02 +0200

    Remove options

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,9 @@
 var http = require('http');
-const url = process.env.BACKEND ? process.env.BACKEND : 'localhost:8080'
+const url = process.env.BACKEND ? process.env.BACKEND : 'http://localhost:8080'
 
 http.createServer(function (req, res) {
-  const options = {
-    host: url,
-    path: '/',
-    method: 'GET'
-  };
 
-  http.request(options, function(res2) {
+  http.request(url, function(res2) {
     res2.setEncoding('utf8');
     res2.on('data', function (chunk) {
 	  res.write(chunk);

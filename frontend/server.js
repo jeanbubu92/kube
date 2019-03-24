bbb3fa73cc36f54c56dcb8ae484f5b616b82ebc8
Jean Bubu <jeanbubu92@gmail.com>
2019-03-24 13:02:33 +0200

    Fix frontend bug

--- frontend/server.js
+++ frontend/server.js
@@ -3,10 +3,11 @@ const url = process.env.BACKEND ? process.env.BACKEND : 'http://localhost:8080'
 
 http.createServer(function (req, res) {
 
-  http.request(url, function(res2) {
+  http.get(url, function(res2) {
     res2.on('data', function (chunk) {
 	  res.write(chunk);
+	  res.end();
     });
   }).end();
-  res.end();
+
 }).listen(8090);

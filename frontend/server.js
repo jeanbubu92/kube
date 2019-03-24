3316284c597781f5accc2b1e1bec57e1000835d7
Jean Bubu <jeanbubu92@gmail.com>
2019-03-24 12:54:52 +0200

    Change the call to backend

--- frontend/server.js
+++ frontend/server.js
@@ -4,10 +4,9 @@ const url = process.env.BACKEND ? process.env.BACKEND : 'http://localhost:8080'
 http.createServer(function (req, res) {
 
   http.request(url, function(res2) {
-    res2.setEncoding('utf8');
     res2.on('data', function (chunk) {
 	  res.write(chunk);
-	  res.end();
     });
   }).end();
+  res.end();
 }).listen(8090);
